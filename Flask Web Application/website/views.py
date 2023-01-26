9a20087c2147bcdb9b2ae43d4995f972bd761df0
sarahkghamd <103235749+sarahkghamd@users.noreply.github.com>
2023-01-26 17:57:57 +0300

    Update views.py

--- Flask Web Application/website/views.py
+++ Flask Web Application/website/views.py
@@ -16,41 +16,39 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", user=current_user)
 
-def videoToframes(file):
-    cap = cv2.VideoCapture(file)
+def videoToframes(interview_id):
+    cap = cv2.VideoCapture('interviews/interview.webm')
     i = 1
     while (cap.isOpened()):
         ret, frame = cap.read()
         if ret == False:
             break
         if i % 1800 == 0:
-            cv2.imwrite('interview/'+str(i)+'.jpg',frame)
-            frame= InterviewFrames()
+            cv2.imwrite('interviews/'+str(interview_id)+'.jpg',frame)
+          #  frame= InterviewFrames() #store in database
             break
         i += 1
     cap.release()
     cv2.destroyAllWindows()
     
-def prediction(data_image):
+def prediction(interview_id):
     facenet_model= DeepFace.build_model('Facenet')
-    facenet_model.load_weights('static/facenet_weights.h5');
+    facenet_model.load_weights('facenet_weights.h5')
     facenet_ext = Model(inputs=facenet_model.input, outputs=facenet_model.get_layer('Mixed_7a').output)
-    model = pickle.load(open('static/INT_Model_1.pkl', 'rb'))
+    model = pickle.load(pickle.read('INT_Model_1.pkl', 'rb'))
 
-    image = data_image # From database
-    image = Image.fromarray(image)  
+    #image = data_image # From database
+    image = Image.fromarray('interviews/'+str(interview_id)+'.jpg')  
 
     # Fit to the model
-    img_dim = (160,160);
+    img_dim = (160,160)
     image = ImageOps.fit(image,(img_dim[0],img_dim[1]),Image.ANTIALIAS,centering=(0.5, 0.5));
     image = np.array(image).reshape(img_dim[1],img_dim[0],3) 
     X = [];
-    X.append(image);
-    X = np.array(X);
+    X.append(image)
+    X = np.array(X)
 
     res= facenet_ext.predict(X)
 
     pred=model.predict(res)
     return(pred)
-
-
